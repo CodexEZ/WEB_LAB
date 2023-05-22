e0725d1948c03d8ede34a5cc02f06d65964f5fb5
CodexEZ <pratapsinghaswin@gmail.com>
2023-05-22 23:44:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router,Link, Route, browserHistory, IndexRoute, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='navbar'>
+        
+          <div><Link to = "/">Home</Link></div>
+          <div><Link to = "/About">About</Link></div>
+          <div><Link to = "/Contact">Contact</Link></div>
+        <Routes>
+                 <Route exact path='/' Component={Home}></Route>
+                 <Route exact path='/About' Component={About}></Route>
+                 <Route exact path='/Contact' Component={Contact}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+class Home extends React.Component{
+  render(){
+    return(
+      <div>
+
+    <h1>Home</h1>
+      </div>
+    )
+  }
+}
+
+
+class About extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>
+          About
+        </h1>
+      </div>
+    )
+  }
+}
+
+
+class Contact extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>
+          Contact
+        </h1>
+      </div>
+    )
+  }
+}
+
